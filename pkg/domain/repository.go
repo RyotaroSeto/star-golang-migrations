56916a8f9f70a3f52689ec6f77f9ff1d8df26481
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-07-15 15:34:39 +0900

    first commit

---
+++ pkg/domain/repository.go
@@ -0,0 +1,13 @@
+package domain
+
+var TargetRepository = []string{
+	"golang-migrate/migrate",
+	"pressly/goose",
+	"amacneil/dbmate",
+	"rubenv/sql-migrate",
+	"k0kubun/sqldef",
+	"schemalex/schemalex",
+	"go-pg/migrations",
+	"naoina/migu",
+	"Boostport/migration",
+}
